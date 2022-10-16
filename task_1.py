40678f4e9a9e72a7394d3bc994fd9811d79149e8
ShajdulinVlad <115501077+ShajdulinVlad@users.noreply.github.com>
2022-10-16 11:44:20 +0300

    lab_3

---
+++ task_1.py
@@ -0,0 +1,11 @@
+src = not False and True or False and not True
+
+#  result = True and True or False and False
+#  result = True or False
+#  result = True
+
+result = True
+
+print(result)
+#  print("Ожидаемый результат: ", src)
+#  print("Мой результат: ", result)
